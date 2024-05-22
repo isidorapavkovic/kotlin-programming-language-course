6395b3ab456c5094eaec88aac23d556c1769e7a6
isidorapavkovic <isidorapavkovic93@gmail.com>
2024-05-22 20:56:58 +0200

    Implemented the solutions to the exercise set 8.

--- src/main/kotlin/exercise8/MovieDB.kt
+++ src/main/kotlin/exercise8/MovieDB.kt
@@ -8,3 +8,69 @@ package exercise8
  * Implement methods defined by MovieDBApi.
  *
  */
+
+internal class MovieDB(private val movies: List<Movie>) : MovieDBApi {
+    override fun getAllMoviesByActor(actor: MovieActor): List<Movie> {
+        return movies.filter { it.actors.contains(actor) }
+    }
+
+    override fun getMoviesWithBiggestProfit(numOfMovies: Int): List<Movie> {
+        return movies.sortedByDescending { it.revenue - it.budget }.take(numOfMovies)
+    }
+
+    override fun getBestRatedMovieByActor(actor: MovieActor): Movie? {
+        return getAllMoviesByActor(actor).maxByOrNull { it.rating }
+    }
+
+    override fun getAllMoviesByYear(year: Int): List<Movie> {
+        return movies.filter { it.releaseDate.year == year }
+    }
+
+    override fun getAllMoviesByGenre(genre: String): List<Movie> {
+        return movies.filter { it.genres.contains(genre) }
+    }
+
+    override fun getBestRatedMovies(numOfMovies: Int): List<Movie> {
+        return movies.sortedByDescending { it.rating }.take(numOfMovies)
+    }
+
+    override fun getDirectorWithMostMoviesDirected(): MovieDirector {
+        val eachCount = movies
+            .groupingBy { it.director }
+            .eachCount()
+
+        return eachCount.maxBy { it.value }.key
+    }
+
+    override fun getActorsWithMostCostarredMovies(): List<Pair<MovieActor, MovieActor>> {
+        val actorToMovies: Map<MovieActor, List<Movie>> = movies
+            .flatMap { movie -> movie.actors.map { actor -> actor to movie } }
+            .groupBy({ actorToMovie -> actorToMovie.first }, { actorToMovie -> actorToMovie.second })
+
+        val actors = actorToMovies
+            .keys
+            .sortedBy { it.name }
+
+        val costarringActors = actors.flatMap { actor ->
+            actors.mapNotNull { anotherActor ->
+                if (actor == anotherActor) return@mapNotNull null
+
+                val (sortedActorOne, sortedActorTwo) = listOf(actor, anotherActor).sortedBy { it.name }
+                val actorOneMovies = actorToMovies[sortedActorOne]?.map { it.title }?.toSet() ?: emptySet()
+                val actorTwoMovies = actorToMovies[sortedActorTwo]?.map { it.title }?.toSet() ?: emptySet()
+                val costarringMovies = actorOneMovies.intersect(actorTwoMovies).count()
+                ActorCostarring(sortedActorOne, sortedActorTwo, costarringMovies)
+            }
+        }.sortedByDescending { it.numOfMovies }
+
+        val maxCostarringMoviesCount = costarringActors.first().numOfMovies
+
+        return costarringActors
+            .takeWhile { it.numOfMovies == maxCostarringMoviesCount }
+            .fold(setOf<Pair<MovieActor, MovieActor>>()) { actorsSet, ( actor, anotherActor, _ ) ->
+                actorsSet + setOf(actor to anotherActor) }.toList()
+    }
+    data class ActorCostarring(val actor: MovieActor,
+                               val anotherActor: MovieActor,
+                               val numOfMovies: Int)
+}
--- src/main/kotlin/exercise8/MovieDBRunner.kt
+++ src/main/kotlin/exercise8/MovieDBRunner.kt
@@ -1,15 +1,49 @@
 package exercise8
 
 import common.FileReader
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 
-private fun parseMovies(moviesLines: List<String>): List<Movie> {
-    TODO("Implement parsing of the file content")
+internal fun parseMovies(moviesLines: List<String>): List<Movie> {
+    val directors = HashMap<String, MovieDirector>()
+    val actors = HashMap<String, MovieActor>()
+
+    return moviesLines
+        .drop(1)
+        .map { movies ->
+            val data = movies.split(";")
+
+            val title = data[2].trim()
+            val budget = data[0].trim().toLong()
+            val revenue = data[5].trim().toLong()
+            val releaseDate = parseDate(data[4])
+            val runtimeInMinutes = data[6].trim().toDouble().toInt()
+            val director = directors.getOrPut(data[8].trim()) { MovieDirector(data[8].trim()) }
+
+            val genres = data[1].trim().split(",")
+                .filter { it.isNotBlank() }
+                .map { it.trim() }
+
+            val movieActors = data[7].trim().split(",")
+                .filter { it.isNotBlank() }
+                .map { actorName -> actors.getOrPut(actorName.trim()) { MovieActor(actorName) } }
+
+            val rating = data[3].trim().toDouble()
+
+            Movie(title, budget, revenue, releaseDate, runtimeInMinutes, director, genres, movieActors, rating)
+        }
+}
+
+private fun parseDate(dateAsString : String) : LocalDate {
+    return LocalDate.parse(dateAsString, DateTimeFormatter.ISO_DATE)
 }
 
 fun main() {
     val moviesCSVFile = FileReader.readFileInResources("exercise8/movies.csv")
     val movies = parseMovies(moviesCSVFile)
 
-    val movieDBApi : MovieDBApi = TODO("Instantiate MovieDB")
+    val movieDBApi : MovieDBApi = MovieDB(movies)
+
+    println(movieDBApi.getBestRatedMovies(5))
 }
